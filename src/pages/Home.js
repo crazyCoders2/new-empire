3c0b77b5face0f11f7b8e84181367531522e670f
Avisekh999 <avisekhgurung8578@gmail.com>
2022-08-07 09:20:39 +0530

    newchanges

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import Navbar from '../components/Navbar'
 import Card from '../components/Card'
 import { getPopularEvents } from '../data'
 import styles from './styles/Home.module.css'
+import Zoom from 'react-reveal/Zoom';
+import Footer from '../components/Footer/Footer'
 
 const HomePage = () => {
   const [popularEvents, setPopularEvents] = useState([])
@@ -17,11 +19,16 @@ const HomePage = () => {
         {/* == Navigation bar == */}
         <Navbar />
         {/* == Header Showcase == */}
-        <div className={styles.showcase}></div>
+        <div className={styles.showcase}>
+          <div>
+            <h1>New Empire Cinema</h1>
+            <h2>The cinema has no boundary; it is a ribbon of dream.</h2>
+          </div>
+        </div>
       </header>
       {/* == Popular Events Section == */}
       <section className={styles.sectionEvents}>
-        <h2 className='heading-secondary'>Popular Events</h2>
+        <h2 className='heading-secondary'>Popular Movies</h2>
         <div className='flex flex-wrap justifyContent-center'>
           {popularEvents.map((event) => {
             return <Card key={event.id} data={event} />
@@ -39,6 +46,29 @@ const HomePage = () => {
           </button>
         </form>
       </section>
+
+       {/* == About Section == */}
+       <section id="aboutUs" className={styles.aboutUs}>
+      <h2 className='heading-secondary'>About Us</h2>
+        <div className={styles.aboutUsBox}>
+          <div className={styles.aboutImgBox}>
+            <Zoom>
+              <img className={styles.aboutImg} src="ticket.png" />
+            </Zoom>
+          </div>
+          <div className={styles.aboutUsTextBox}>
+            <h2 style={{ color: '#fff', marginTop:'3rem'}} className='heading-tertiary'>Description</h2>
+            <h3 style={{ color: '#fff' }} className='text'>
+              New Empire Cinema is a single screen cinema hall located in Humayun Place in New Market, (opposite Shreeram Arcade), Kolkata, West Bengal, India.
+            </h3>
+            <h3
+              style={{ color: '#fff' }} className='text'
+            >
+              It was established in the late 1950s under the chairmenship of V. Shantaram, the First International New Youth Film was held in New Empire.
+            </h3>
+          </div>
+        </div>
+      </section>
       {/* == Features Section == */}
       <section className={styles.sectionFeatures}>
         <h2 className='heading-secondary'>Why Book With Us?</h2>
@@ -54,6 +84,12 @@ const HomePage = () => {
           <div className={styles.sectionFeatures__bannerImage}></div>
         </div>
       </section>
+
+      {/* == Footer == */}
+      <Footer />
+      
+
+     
     </>
   )
 }
